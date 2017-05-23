c07734c4a9e9957b07136e390be1531e8363d24c
John Ivan <johnbauzon@gmail.com>
2017-05-23 15:19:34 -0700

    cookie and session progress

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const path = require('path');
 const userController = require('./controllers/userController');
 const recipeController = require('./controllers/recipeController');
 const dayController = require('./controllers/dayController');
+const cookieController = require('./controllers/cookieController');
+const sessionController = require('./controllers/sessionController');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/public')));
 
 app.post('/login', userController.verifyUser);
-app.post('/signup', userController.checkIfUsernameExists, 
-                    userController.addToUsersTable, 
+app.post('/signup', cookieController.setCookie,
+                    userController.checkIfUsernameExists,
+                    userController.addToUsersTable,
                     userController.createUserTable);
 app.post('/recipeDisplay', recipeController.saveRecipe);
 app.get('/day/:day/:username', dayController.getRowsForDay);//req.params.day /monday/doug
