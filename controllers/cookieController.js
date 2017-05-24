01f3ed900e555f4b7e42d5df3565ff5a0aa03c7a
John Ivan <johnbauzon@gmail.com>
2017-05-23 22:15:17 -0700

    cookies setup v1

--- controllers/cookieController.js
+++ controllers/cookieController.js
@@ -3,8 +3,9 @@ cookieController.setCookie = setCookie;
 
 function setCookie(req, res, next) {
   console.log('COOKIE BODY', req.body);
-  res.cookie('id', req.body._id, { httpOnly: true });
-  next();
+  res.cookie('futureMeals', req.body.id, { httpOnly: true })
+      res.status(200);
+      res.end();
 }
 
 module.exports = cookieController;
