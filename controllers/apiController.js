d1d95e6f4f8bf8d0d4f0c8fbef35818da219c2cf
John Ivan <johnbauzon@gmail.com>
2017-05-24 13:49:07 -0700

    pre merge

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 const find = (req, res, next) => {
   const base = 'https://api.edamam.com/search?';
-  const q = `q=${req.query}`;
+  const q = `q=${req.query.query}`;
   const idAndKey = '&app_id=9c91d5f4&app_key=6f47ee6858565edebe96788f8743461a';
   const range = '&from=0&to=5';
   const url = base + q + idAndKey + range;
